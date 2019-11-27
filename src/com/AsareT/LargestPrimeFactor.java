72374e138791909ccbc5568f07aef5c1a5d1182a
Asare Twum <Asare_T@hotmail.com>
2019-11-27 16:33:17 +0100

    Solved Problem 3 with a hint from: https://www.math-only-math.com/methods-of-prime-factorization.html

--- src/com/AsareT/LargestPrimeFactor.java
+++ src/com/AsareT/LargestPrimeFactor.java
@@ -4,13 +4,13 @@ public class LargestPrimeFactor {
 
     public static long largestPrimeFactorOf(long num){
         long largest = 0;
-
-        for ( long i = 1; i < num; i++ ){
-            if ( num % i == 0 && isPrime(i)){
-                largest = i;
+        //uses the division approach to finding the largest prime number
+        for ( long i = 3; i < num; i++ ){
+            while ( num % i == 0 ){
+                num = num / i;
             }
         }
-        return largest;
+        return num;
     }
 
 
@@ -18,7 +18,7 @@ public class LargestPrimeFactor {
         if ( num == 2 || num == 3){
             return true;
         }
-        for ( int i = 2; i < num; i++){
+        for ( int i = 3; i < num; i++){
             if ( (num % i) == 0){
                 return false;
             }
